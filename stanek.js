d32537b95ccfe011bd8227b6af3d36dba2cf1c29
Zelow79 <zelow79@gmail.com>
2023-02-15 14:31:15 -0500

    Create stanek.js
    loads layouts from a text or js file in the `/giftLayouts/` directory.

---
+++ stanek.js
@@ -0,0 +1,15 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	ns.clearLog(); ns.disableLog("ALL");
+	const s = ns.stanek
+	const layoutFile = await ns.prompt("Load which layout?", { type: "select", choices: ns.ls("home", "/giftLayouts/") });
+	if (!ns.ls("home", "/giftLayouts/").includes(layoutFile)) ns.exit();
+	const layout = JSON.parse(ns.read(layoutFile));
+
+	if (s.acceptGift() && s.activeFragments().length == 0) {
+		for (const frag of layout) {
+			if (s.placeFragment(frag.x, frag.y, frag.rotation, frag.id)) ns.print(`Fragment-#${frag.id} set in location X: ${frag.x}, Y: ${frag.y} & Rotation: ${frag.rotation}`)
+		}
+	}
+	ns.tail(); ns.resizeTail(450, 600);
+}
