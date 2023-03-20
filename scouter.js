c2293d3b5d4b9987bd25538cb0d58232b47717ed
Zelow79 <zelow79@gmail.com>
2023-03-19 23:19:00 -0400

    Update scouter.js
    cleaned up various lines of code and moved in library functions where applicable

--- scouter.js
+++ scouter.js
@@ -1,64 +1,49 @@
-import { hmsms, format, formatGB } from "ze-lib.js";
+import { getAllServers, hmsms, format, formatGB, formatPercent, bar } from "ze-lib.js";
 /** @param {NS} ns */
 export async function main(ns) {
 	ns.clearLog(); ns.disableLog("ALL"); //ns.enableLog("sleep");
-	let targets = ["home"] // Leave this "home" used for funcion
+	let targets = getAllServers(ns);
 	const sleepTime = 200
-	function scanner(a) { // This function gets all the servers and puts them in the array targets
-		for (let x = 0; x < a.length; x++) {
-			let y = ns.scan(a[x]);
-			a = Array.from(new Set(a.concat(y)));
-		}
-		return a;
-	}
-	targets = scanner(targets); // runs above scanner function
 	let getInput = ns.args[0] ?? await ns.prompt("Select Server", { type: "select", choices: targets });
 	ns.tail();
 	while (true) {
-		ns.resizeTail(275, 385);
+		ns.resizeTail(252, 390);
 		const serverStats = ns.getServer(getInput);
-		const progress = Math.floor(serverStats.moneyAvailable / serverStats.moneyMax * 100);
+		const progress = serverStats.moneyAvailable / serverStats.moneyMax;
 		const freeRam = serverStats.maxRam - serverStats.ramUsed
-		const rProgress = Math.floor(freeRam / serverStats.maxRam * 100);
-		const progressBarMessage = "░"
-		const progressBar = progressBarMessage.repeat(Math.floor(progress * 0.25));
-		const rProgressBar = progressBarMessage.repeat(Math.floor(rProgress * 0.25));
-		const openPortMessage = "Δ"
-		let sshPort = " "
-		let ftpPort = " "
-		let smptPort = " "
-		let httpPort = " "
-		let sqlPort = " "
-		if (serverStats.sshPortOpen == true) { sshPort = openPortMessage }
-		if (serverStats.ftpPortOpen == true) { ftpPort = openPortMessage }
-		if (serverStats.smptPortOpen == true) { smptPort = openPortMessage }
-		if (serverStats.httpPortOpen == true) { httpPort = openPortMessage }
-		if (serverStats.sqlPortOpen == true) { sqlPort = openPortMessage }
+		const rProgress = freeRam / serverStats.maxRam;
+		let [sshPort, ftpPort, smtpPort, httpPort, sqlPort] = [
+			serverStats.sshPortOpen ? "Δ" : " ",
+			serverStats.ftpPortOpen ? "Δ" : " ",
+			serverStats.smtpPortOpen ? "Δ" : " ",
+			serverStats.httpPortOpen ? "Δ" : " ",
+			serverStats.sqlPortOpen ? "Δ" : " "
+		]
 		ns.clearLog();
-		ns.print("Server Name:        " + serverStats.hostname);
-		ns.print("IP Adress:          " + serverStats.ip);
-		ns.print("Base Security:      " + format(serverStats.baseDifficulty));
-		ns.print("Min Security:       " + format(serverStats.minDifficulty));
-		ns.print("Security Level:     " + format(serverStats.hackDifficulty));
-		ns.print(`Hack Time:          ${hmsms(ns.getHackTime(getInput))}`)
-		ns.print("Required Ports:     " + format(serverStats.numOpenPortsRequired));
-		ns.print("Max Server Money:   $" + format(serverStats.moneyMax));
+		ns.print("Server Name:      " + serverStats.hostname.slice(0, 12));
+		ns.print("IP Adress:        " + serverStats.ip);
+		ns.print("Base Security:    " + format(serverStats.baseDifficulty));
+		ns.print("Min Security:     " + format(serverStats.minDifficulty));
+		ns.print("Security:         " + format(serverStats.hackDifficulty));
+		ns.print(`Hack Time:        ${hmsms(ns.getHackTime(getInput))}`)
+		ns.print("Required Ports:   " + format(serverStats.numOpenPortsRequired));
+		ns.print("Max Money:        $" + format(serverStats.moneyMax));
 		if (serverStats.moneyMax > 0) {
-			ns.print("Current Money:      $" + format(serverStats.moneyAvailable));
+			ns.print("Current Money:    $" + format(serverStats.moneyAvailable));
 			ns.print("Money Progress Bar");
-			ns.print(progressBar + "| " + progress + "%");
+			ns.print(bar(progress, "⚡", 23) + "|" + formatPercent(progress, 0));
 		}
-		ns.print("Max Ram:            " + formatGB(serverStats.maxRam * 1e9));
+		ns.print("Max Ram:          " + formatGB(serverStats.maxRam * 1e9));
 		if (serverStats.maxRam > 0) {
-			ns.print("Used Ram:           " + formatGB(serverStats.ramUsed * 1e9));
+			ns.print("Used Ram:         " + formatGB(serverStats.ramUsed * 1e9));
 			ns.print("Free Ram Progress Bar");
-			ns.print(rProgressBar + "| " + rProgress + "%");
+			ns.print(bar(rProgress, "⚡", 23) + "|" + formatPercent(rProgress, 0));
 		}
 		ns.print("╒═════════════════════════════╕");
 		ns.print("│         OPEN PORTS          │");
 		ns.print("╞═════╤═════╤═════╤═════╤═════╡");
-		ns.print("│ SSH │ FPT │ SMP │ HTT │ SQL │");
-		ns.print("│  " + sshPort + "  │  " + ftpPort + "  │  " + smptPort + "  │  " + httpPort + "  │  " + sqlPort + "  │");
+		ns.print("│ SSH │ FPT │ SMT │ HTT │ SQL │");
+		ns.print("│  " + sshPort + "  │  " + ftpPort + "  │  " + smtpPort + "  │  " + httpPort + "  │  " + sqlPort + "  │");
 		ns.print("╘═════╧═════╧═════╧═════╧═════╛");
 		await ns.sleep(sleepTime);
 	}
