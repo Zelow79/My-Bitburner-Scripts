1e2ab2f35a2039c4c998f7d1dda12a3cb5b4cd2a
Zelow79 <zelow79@gmail.com>
2023-03-28 17:53:42 -0400

    Update sharingIsCaring.js
    changed a few things around added excluded servers to print out

--- sharingIsCaring.js
+++ sharingIsCaring.js
@@ -2,16 +2,16 @@ import { shareIt, getAllServers, format, formatPercent } from "ze-lib"
 /** @param {NS} ns */
 export async function main(ns) {
 	ns.clearLog(); ns.disableLog("ALL"); ns.tail();
-	const [pids, excludes] = [[], []]
+	const [pids, excludes, servers] = [[], [], getAllServers(ns)]
 	ns.args.forEach(a => {
 		if (ns.serverExists(a)) excludes.push(a);
 	});
-	for (const server of getAllServers(ns)) {
-		if (excludes.includes(server)) continue;
-		const p = shareIt(ns, server);
-		if (p === null) continue;
-		pids.push(p);
-	}
+	servers.forEach(server => {
+		if (!excludes.includes(server)) {
+			const p = shareIt(ns, server);
+			if (p !== null) pids.push(p);
+		}
+	});
 	ns.atExit(() => {
 		const terminated = []
 		pids.forEach(p => {
@@ -26,9 +26,11 @@ export async function main(ns) {
 			if (ns.getRunningScript(p) === null) pids.splice(pids.indexOf(p), 1);
 			else threadCount += ns.getRunningScript(p).threads
 		});
-		ns.print(`Number of share scripts running: ${pids.length}`);
-		ns.print(`Total threads allocated:         ${format(threadCount)}`);
-		ns.print(`Total share power:               ${formatPercent(ns.getSharePower() - 1)}`);
+		const message = [`Active share scripts: ${pids.length}`]
+		message.push(`Total threads:        ${format(threadCount)}`);
+		message.push(`Total share power:    ${formatPercent(ns.getSharePower() - 1)}`);
+		message.push(`Excluded Servers:     ${excludes.join(", ")}`);
+		ns.print(message.join("\n"));
 		await ns.sleep(0);
 	}
 }
