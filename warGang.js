0a71562572440dfd2c277d68ac64b4dfa620d2fd
Zelow79 <zelow79@gmail.com>
2023-04-04 05:22:09 -0400

    Update warGang.js
    few more tweaks, crunched a few more lines

--- warGang.js
+++ warGang.js
@@ -4,13 +4,8 @@ const [discountThresh, wantedPenThresh] = [0.8, 0.05]
 export async function main(ns) {
 	ns.disableLog("ALL"); ns.clearLog(); ns.tail();
 	const [faction, width, height] = ["Slum Snakes", 390, 888]
-	const tick = {
-		tw: false,
-		otherGangsInfoPrevCycle: undefined,
-		nextTick: undefined
-	}
-	await ns.sleep(500);
-	ns.resizeTail(width, height);
+	const tick = { tw: false, otherGangsInfoPrevCycle: undefined, nextTick: undefined }
+	await ns.sleep(500); // sleep here to all first ns.resizeTail() to work properly
 	while (true) {
 		ns.resizeTail(width, height); ns.clearLog();
 		if (ns.gang.inGang()) {
