7b6d95e5e0ac70c3d854c84d411a0628e6c18dc9
Zelow79 <zelow79@gmail.com>
2023-04-16 20:26:47 -0400

    Update nuke.js
    smoosh + added try catch for ns.nuke incase it can't open enough ports

--- nuke.js
+++ nuke.js
@@ -1,16 +1,9 @@
 export const main = async (ns) => {
-	const target = ns.args[0] ?? "n00dles"
-	const cracks = [{ name: "BruteSSH", doIt: () => ns.brutessh(target) },
-	{ name: "FTPCrack", doIt: () => ns.ftpcrack(target) },
-	{ name: "relaySMTP", doIt: () => ns.relaysmtp(target) },
-	{ name: "HTTPWorm", doIt: () => ns.httpworm(target) },
-	{ name: "SQLInject", doIt: () => ns.sqlinject(target) }]
-	cracks.forEach(crack => {
-		cracker(crack.name + ".exe");
-		if (ns.ls("home").includes(crack.name + ".exe")) crack.doIt();
-	});
-	ns.nuke(target);
-	function cracker(crack) {
-		if (ns.singularity.purchaseTor()) ns.singularity.purchaseProgram(crack);
-	}
+	const target = ns.args[0] ?? "n00dles",
+		cracks = [{ name: "BruteSSH", doIt: () => ns.brutessh(target) }, { name: "FTPCrack", doIt: () => ns.ftpcrack(target) },
+		{ name: "relaySMTP", doIt: () => ns.relaysmtp(target) }, { name: "HTTPWorm", doIt: () => ns.httpworm(target) },
+		{ name: "SQLInject", doIt: () => ns.sqlinject(target) }],
+		cracker = (crack) => ns.singularity.purchaseTor() ? ns.singularity.purchaseProgram(crack) : null;
+	cracks.forEach(crack => { cracker(crack.name + ".exe"); if (ns.ls("home").includes(crack.name + ".exe")) crack.doIt(); });
+	try { ns.nuke(target); } catch { ns.print(`nuke failed on ${target}.`); }
 }
