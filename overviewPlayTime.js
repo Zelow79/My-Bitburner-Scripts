363384f323edd17c848b8ea628827c00f7c5a28d
Zelow79 <zelow79@gmail.com>
2023-04-24 01:12:04 -0400

    Update overviewPlayTime.js
    cleaned up some of the code a little bit

--- overviewPlayTime.js
+++ overviewPlayTime.js
@@ -1,20 +1,17 @@
 import { dhms } from "ze-lib.js"
 export async function main(ns) {
-  ns.clearLog(); ns.disableLog("sleep");
-  const doc = eval('document');
-  let hook0 = doc.getElementById('overview-extra-hook-0');
-  let hook1 = doc.getElementById('overview-extra-hook-1');
-  ns.atExit(() => {
-    hook0.innerText = ""
-    hook1.innerText = ""
-  });
-  while (1) {
-    const player = ns.getPlayer();
-    const totalPlaytime = player.totalPlaytime
-    const lastBitnode = player.playtimeSinceLastBitnode
-    const lastAug = player.playtimeSinceLastAug
-    hook0.innerText = `Playtime\nLast Aug:\nBN clear:\nTotal:`
-    hook1.innerText = `\n${dhms(lastAug)}\n${dhms(lastBitnode)}\n${dhms(totalPlaytime)}`
-    await ns.sleep(1000);
-  }
+	ns.clearLog(); ns.disableLog("sleep");
+	const doc = eval('document'),
+		hook0 = doc.getElementById('overview-extra-hook-0'),
+		hook1 = doc.getElementById('overview-extra-hook-1');
+	ns.atExit(() => { hook0.innerText = "", hook1.innerText = "" });
+	while (1) {
+		const player = ns.getPlayer(),
+			totalPlaytime = player.totalPlaytime,
+			lastBitnode = player.playtimeSinceLastBitnode,
+			lastAug = player.playtimeSinceLastAug;
+		hook0.innerText = `Playtime\nLast Aug:\nBN clear:\nTotal:`,
+			hook1.innerText = `\n${dhms(lastAug)}\n${dhms(lastBitnode)}\n${dhms(totalPlaytime)}`;
+		await ns.sleep(1000);
+	}
 }
