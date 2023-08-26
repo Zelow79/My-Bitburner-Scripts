16450d93e18894dd6e79fcd4bfc7d9ee1ea7585e
Zelow79 <zelow79@gmail.com>
2023-08-25 20:10:32 -0400

    Update ze-lib.js
    -Tweaked getAllServers() to reduce line count.
    -Tweaked hgw() to report arguments used to console log.
    -Added cipher/decipher function for string encryption.

--- ze-lib.js
+++ ze-lib.js
@@ -2,11 +2,7 @@ export const cities = ["Sector-12", "Aevum", "Volhaven", "Chongqing", "New Tokyo
 
 export function getAllServers(ns, homeless = false) {
 	const x = new Set(["home"]);
-	for (const server of x) {
-		for (const connectServer of ns.scan(server)) {
-			x.add(connectServer);
-		}
-	}
+	x.forEach(server => ns.scan(server).forEach(connectServer => x.add(connectServer)));
 	if (homeless) x.delete("home");
 	return Array.from(x);
 }
@@ -18,11 +14,13 @@ export function pathFinder(ns, server) {
 }
 
 export function hgw(ns, serverName, host = "home", sleepTime = 0, pingPort = false) {
+	const ahhgs = [serverName, sleepTime, pingPort];
+	console.log("args:"); console.log(ahhgs);
 	return {
 		nukeIt: () => ns.exec("nuke.js", "home", 1, serverName),
-		hackIt: (t = 1) => ns.exec("hack.js", host, t, serverName, sleepTime, pingPort),
-		growIt: (t = 1) => ns.exec("grow.js", host, t, serverName, sleepTime, pingPort),
-		weakIt: (t = 1) => ns.exec("weaken.js", host, t, serverName, sleepTime, pingPort)
+		hackIt: (t = 1) => ns.exec("hack.js", host, t, ...ahhgs),
+		growIt: (t = 1) => ns.exec("grow.js", host, t, ...ahhgs),
+		weakIt: (t = 1) => ns.exec("weaken.js", host, t, ...ahhgs)
 	};
 }
 
@@ -226,6 +224,40 @@ export function playSound(url) {
 	const audio = new Audio();
 	audio.src = url;
 	audio.play();
+}
+
+export function cipher(word, code = [420]) { //word to be encrypted and array of secret codes for encryption
+	if (!word || code.length < 1) return null;
+
+	function process(w, c) {
+		const encrypted = []; //storage for encryption
+		for (let i = 0; i < w.length; i++) { //iterate word letter by letter
+			encrypted.push(w.charCodeAt(i) ^ c); //convert letter to char number and xor bit shift by code
+		}
+		return String.fromCharCode(...encrypted) //return converted char numbers back to string
+	}
+	let result = word; //result will be modified based on number of codes given in code array
+	for (let i = 0; i < code.length; i++) {
+		result = process(result, code[i]);
+	}
+	return result;
+}
+
+export function decipher(word, code = [420]) { //word to be decrypted and array of secret codes for decryption
+	if (!word || code.length < 1) return null;
+
+	function process(w, c) {
+		const decrypted = []; //storage for decryption
+		for (let i = 0; i < w.length; i++) { //iterate encryptedWord letter by letter
+			decrypted.push(w.charCodeAt(i) ^ c); //convert letter to char number and xor bit shift by code
+		}
+		return String.fromCharCode(...decrypted); //return converted char numbers back to string
+	}
+	let result = word; //result will be modified based on number of codes given in code array
+	for (let i = 0; i < code.length; i++) {
+		result = process(result, code[i]);
+	}
+	return result;
 }
 
 export function tableMaker(data, columns = 4, tableProps = null, tdProps = null) {  // data is an array of Numbers or Strings
