df6a5271d29e26cbd851805be83b4c5bfb359928
Zelow79 <zelow79@gmail.com>
2023-08-25 21:50:14 -0400

    Code Change
    Changed the code I use for default.

--- cipher.js
+++ cipher.js
@@ -7,7 +7,7 @@ export async function main(ns) {
 	}
 
 	let code = typeof ns.args[1] === "string" ? JSON.parse(ns.args[1]) :
-		ns.args[1] ?? [34, 19, 57, 88, 20, 81, 66, 44, 101, 29]; //needs an array of 1 or more codes
+		ns.args[1] ?? [304, 39, 57, 88, 20, 81, 66, 44, 101, 79]; //needs an array of 1 or more codes
 	ns.tprintRaw([React.createElement("span", { style: { color: "white" } }, "Encrypted: "),
 	React.createElement("span", { style: { color: "lightblue" } }, cipher(ns.args[0], code))]); //print encrypted code
 }
