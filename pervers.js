7a0268aff77376002823710d41d1e7261821e7e6
Zelow79 <zelow79@gmail.com>
2023-09-20 23:30:14 -0400

    Update pervers.js
    minor writing style changes.

--- pervers.js
+++ pervers.js
@@ -8,10 +8,10 @@ export async function main(ns) {
 			ns.purchaseServer(`perver-${numPad(ns.getPurchasedServers().length, 2)}`, 2);
 		}
 		for (const perver of ns.getPurchasedServers()) {
-			const limit = isNaN(ns.args[0]) || ns.args[0] === true ? true : ns.getServer(perver).maxRam < ns.args[0]
-			const smallerExists = ns.getPurchasedServers().some(s => ns.getServer(s).maxRam < ns.getServer(perver).maxRam);
-			const canUpgrade = ns.getServer(perver).maxRam < ns.getPurchasedServerMaxRam();
-			const canAfford = ns.getPurchasedServerUpgradeCost(perver, ns.getServer(perver).maxRam * 2) <= ns.getPlayer().money;
+			const limit = isNaN(ns.args[0]) || ns.args[0] === true ? true : ns.getServer(perver).maxRam < ns.args[0],
+				smallerExists = ns.getPurchasedServers().some(s => ns.getServer(s).maxRam < ns.getServer(perver).maxRam),
+				canUpgrade = ns.getServer(perver).maxRam < ns.getPurchasedServerMaxRam(),
+				canAfford = ns.getPurchasedServerUpgradeCost(perver, ns.getServer(perver).maxRam * 2) <= ns.getPlayer().money;
 			if (limit && !smallerExists && canUpgrade && canAfford) ns.upgradePurchasedServer(perver, ns.getServer(perver).maxRam * 2);
 		}
 		ns.clearLog();
@@ -21,8 +21,8 @@ export async function main(ns) {
 			const cost = () => ns.getPurchasedServerUpgradeCost(perver, ns.getServer(perver).maxRam * 2);
 			ns.print(`${perver} RAM: ${formatGB(ns.getServer(perver).maxRam * 1073741824)} Upgrade Cost: ${format(cost())}`);
 		}
-		if (!isNaN(ns.args[0]) && ns.getPurchasedServers().every(s => ns.getServer(s).maxRam === ns.args[0])) ns.exit();
-		if (ns.getPurchasedServers().every(s => ns.getServer(s).maxRam === ns.getPurchasedServerMaxRam())) ns.exit();
+		if (!isNaN(ns.args[0]) && ns.getPurchasedServers().every(s => ns.getServer(s).maxRam === ns.args[0])
+			|| ns.getPurchasedServers().every(s => ns.getServer(s).maxRam === ns.getPurchasedServerMaxRam())) ns.exit();
 		await ns.sleep(10);
 	}
 }
