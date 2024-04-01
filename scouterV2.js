c395e1bfe8251c4883be1636aca2f022fe5b3afd
Zelow79 <zelow79@gmail.com>
2024-03-31 23:55:36 -0400

    Update scouterV2.js
    Changed some sizing and removed auto margin.

--- scouterV2.js
+++ scouterV2.js
@@ -3,7 +3,7 @@ import { tableMaker, format, getAllServers } from "ze-lib";
 export async function main(ns) {
 	const content = [], inject = (data, style) => React.createElement("div", { style }, data),
 		target = ns.args[0] ?? await ns.prompt("Select Server", { type: "select", choices: getAllServers(ns) });
-	ns.clearLog(); ns.tail(); ns.resizeTail(600, 450);
+	ns.clearLog(); ns.tail(); ns.resizeTail(620, 450);
 
 	for (const [key, value] of Object.entries(ns.getServer(target))) {
 		if (value === null || value === undefined) continue;
@@ -11,6 +11,6 @@ export async function main(ns) {
 		content.push(inject(typeof value === "number" ? format(value) : String(value), { color: "white", "text-align": "center" }));
 	}
 
-	ns.printRaw(React.createElement("div", { style: { margin: "auto", width: "100%" } }, React.createElement("img", { style: { margin: "auto", display: "block", width: "50%" }, src: "https://raw.githubusercontent.com/Zelow79/My-Bitburner-Scripts/main/resources/vegeta-scouter.gif" })));
-	ns.printRaw(tableMaker(content, 4, { style: { border: "1px solid white", "border-collapse": "collapse", width: "90%", margin: "auto" } }, { style: { border: "1px solid white" } }));
+	ns.printRaw(React.createElement("div", { style: { width: "600px" } }, React.createElement("img", { style: { margin: "auto", display: "block", width: "50%" }, src: "https://raw.githubusercontent.com/Zelow79/My-Bitburner-Scripts/main/resources/vegeta-scouter.gif" })));
+	ns.printRaw(tableMaker(content, 4, { style: { border: "1px solid white", "border-collapse": "collapse", width: "600px" } }, { style: { border: "1px solid white" } }));
 }
