b98afc58f6f6626237386f33ec986d73621308c0
Zelow79 <zelow79@gmail.com>
2024-04-14 18:44:56 -0400

    Create graft.js
    First attempt at a grafting script. Wasn't really sure what I wanted exactly when I started it, but I think I still managed to end up pretty happy with it. Might see some changes in the future as I notice issues or potential bugs. or find a reason to add more modes.

---
+++ graft.js
@@ -0,0 +1,139 @@
+import { hms } from "ze-lib";
+/** @param {NS} ns */
+export async function main(ns) {
+	ns.disableLog("sleep"); ns.clearLog();
+	const mode = {
+		["hacking"]: ns.args.includes("hacking") || ns.args.includes("hack") ? true : false, // a pool of augs that effect hacking
+		["hacknet"]: ns.args.includes("hacknet") ? true : false, // augs that include hacknet cost/profits
+		combat: ns.args.includes("combat") ? true : false, // combat stat augs str, def, dex, agi
+		bb: ns.args.includes("bb") ? true : false, // bb augs analysis, max_stamina etc
+		cha: ns.args.includes("cha") || ns.args.includes("charisma") ? true : false, // charisma
+		comprep: ns.args.includes("company") || ns.args.includes("rep") ? true : false, // company reputation gain augs
+		factionrep: ns.args.includes("faction") || ns.args.includes("rep") ? true : false, // faction rep gain and rep for both comp and faction
+		["work"]: ns.args.includes("work") ? true : false, // work money gained
+		all: ns.args.includes("all") ? true : false, // will attempt from a pool of any augments
+		fast: ns.args.includes("fast") ? true : false, // sort augs fastest first *try to only use one sort at a time
+		cheap: ns.args.includes("cheap") ? true : false, // sort augs cheapest first *default is the most expensive first
+		["super"]: ns.args.includes("super") ? true : false // super mode is intended to take priority over other tasks
+	}
+
+	while (1) {
+		await ns.sleep(0); // Safety sleep
+		const priority = ["Neuroreceptor Management Implant", "The Blade's Simulacrum", "nickofolas Congruity Implant"],
+			remaining = [], graftableAugs = ns.grafting.getGraftableAugmentations();
+
+		for (const aug of priority) {
+			if (aug === "The Blade's Simulacrum") { // check for Bladeburners faction or mode before allowing Simulacrum
+				if (!mode.bb || !ns.getPlayer().factions.includes("Bladeburners")) continue;
+			}
+			if (!ns.singularity.getOwnedAugmentations(true).includes(aug)) remaining.push(aug);
+		}
+
+		const start = performance.now();
+		while (ns.singularity.getCurrentWork() !== null) { // basically sleep until idle
+			ns.clearLog();
+			if (ns.singularity.getCurrentWork() !== null && ns.singularity.getCurrentWork().type === "GRAFTING") {
+				const task = ns.singularity.getCurrentWork(),
+					graftTime = Math.ceil(ns.grafting.getAugmentationGraftTime(task.augmentation));
+				ns.printRaw("Type:          " + task.type); // if current task is grafting display some information
+				ns.printRaw("Aug:           " + task.augmentation);
+				ns.printRaw("Graft Time:    " + hms(graftTime));
+				ns.printRaw("Graft Left:    " + hms(graftTime - task.cyclesWorked * 200));
+				ns.printRaw("Time Passed:   " + hms(task.cyclesWorked * 200));
+				ns.printRaw("Cycles Worked: " + task.cyclesWorked);
+			} else {
+				ns.printRaw("Current Work: " + ns.singularity.getCurrentWork()); // information for tasks that aren't grafting
+				ns.print("Not Grafting idle for " + (Math.floor(performance.now() - start)) + " ms");
+				if (mode["super"]) ns.singularity.stopAction(); // stop current task so we can start grafting when super mode is on
+			}
+			await ns.sleep(1000);
+		}
+
+		if (remaining.length === 0) {
+			//code for rest of graftables
+			ns.clearLog();
+			ns.printRaw("All priority augments already installed or grafted.");
+			ns.printRaw(graftableAugs.length + " graftable augs remain.");
+
+			for (const aug of getCrackAugs()) {
+				if (ns.getPlayer().money > aug.cost) {
+					ns.tprintRaw(`Grafting ${aug.name} for ${aug.cost} will take ${Math.ceil(aug.graftTime)}ms`);
+					ns.grafting.graftAugmentation(aug.name, !ns.singularity.getOwnedAugmentations(false).includes("Neuroreceptor Management Implant"));
+					break;
+				} else {
+					ns.printRaw(`Unable to graft ${aug.name} cost: ${aug.cost} - Not enough money.`)
+				}
+			}
+		} else {
+			if (ns.getPlayer().money > ns.grafting.getAugmentationGraftPrice(remaining[0])) {
+				ns.grafting.graftAugmentation(remaining[0], !ns.singularity.getOwnedAugmentations(false).includes("Neuroreceptor Management Implant"));
+			} else {
+				ns.printRaw(`Not enough money to graft ${remaining[0]} Cost: ${ns.grafting.getAugmentationGraftPrice(remaining[0])}`);
+				ns.exit();
+			}
+		}
+	}
+
+	function getCrackAugs(graftableAugs = ns.grafting.getGraftableAugmentations()) {
+		const chosen = [];
+		for (const a of graftableAugs) {
+			const preReqs = ns.singularity.getAugmentationPrereq(a);
+			if (preReqs.length > 0) { // check if there is preReqs for the aug
+				const actuallyNeed = []; // create container for augs we really need
+				for (const p of preReqs) { // iterate list of potential augs needed
+					if (ns.singularity.getOwnedAugmentations(true).includes(p)) continue; // skip if we got it
+					actuallyNeed.push(p); // push aug into actuallyNeed 
+				}
+				if (actuallyNeed.length > 0) continue; // skip a if preReqs are still needed
+			}
+
+			const o = {
+				name: a,
+				cost: ns.grafting.getAugmentationGraftPrice(a),
+				stats: ns.singularity.getAugmentationStats(a),
+				graftTime: ns.grafting.getAugmentationGraftTime(a),
+				preReqs: ns.singularity.getAugmentationPrereq(a)
+			}
+
+			const bbwant = mode.bb && (o.stats.bladeburner_analysis > 1
+				|| o.stats.bladeburner_max_stamina > 1
+				|| o.stats.bladeburner_stamina_gain > 1
+				|| o.stats.bladeburner_success_chance > 1),
+				hackwant = mode["hacking"] && (o.stats["hacking"] > 1
+					|| o.stats.hacking_chance > 1
+					|| o.stats.hacking_exp > 1
+					|| o.stats.hacking_speed > 1
+					|| o.stats.hacking_money > 1
+					|| o.stats.hacking_grow > 1),
+				combatwant = mode.combat && (o.stats["strength"] > 1
+					|| o.stats.strength_exp > 1
+					|| o.stats["defense"] > 1
+					|| o.stats.defense_exp > 1
+					|| o.stats["dexterity"] > 1
+					|| o.stats.dexterity_exp > 1
+					|| o.stats["agility"] > 1
+					|| o.stats.agility_exp > 1),
+				chawant = mode.cha && (o.stats.charisma > 1
+					|| o.stats.charisma_exp > 1),
+				hacknetwant = mode["hacknet"] && (o.stats.hacknet_node_money > 1
+					|| o.stats.hacknet_node_purchase_cost < 1
+					|| o.stats.hacknet_node_ram_cost < 1
+					|| o.stats.hacknet_node_level_cost < 1
+					|| o.stats.hacknet_node_core_cost < 1),
+				comprepwant = mode.comprep && o.stats.company_rep > 1,
+				factionrepwant = mode.factionrep && o.stats.faction_rep > 1,
+				workwant = mode["work"] && o.stats.work_money > 1;
+
+			if (mode.all || bbwant || hackwant || combatwant || chawant || hacknetwant || comprepwant || factionrepwant || workwant) chosen.push(o);
+		}
+
+		if (mode.cheap) {
+			chosen.sort((a, b) => a.cost - b.cost); // if cheap mode is on, sort by cheapest first
+		} else if (mode.fast) {
+			chosen.sort((a, b) => a.graftTime - b.graftTime); // if fast mode is on shortest time first
+		} else {
+			chosen.sort((a, b) => b.cost - a.cost); // most expensive first by default
+		}
+		return chosen;
+	}
+}
