cbc657c5a69ddc711851baa8c2447ac29e19a6cf
Zelow79 <zelow79@gmail.com>
2024-04-29 14:33:22 -0400

    charge overhaul
    removed spawn for an active charger, now takes all charge workers with it on exit and timestamps when that happens.
    added arg option to call ai.js (my personal start script)

--- chargeRest.js
+++ chargeRest.js
@@ -1,22 +1,45 @@
 import { getAllServers } from "ze-lib.js";
 /** @param {NS} ns */
 export async function main(ns) {
-	ns.disableLog('sleep'); ns.clearLog(); const fileName = "charge_marked.js"; ns.exec("superNuker.js", "home");
-	while (ns.isRunning("superNuker.js", ns.getHostname())) await ns.sleep(0); ns.tail(); doIt(fileName);
+	ns.disableLog('sleep'); ns.clearLog();
+	const fileName = "charge_marked.js",
+		startTime = performance.now(),
+		workers = [];
 
-	function doIt(fileName) {
-		const fileSize = ns.getScriptRam(fileName), hns = [],
-			killIt = (server, fileName) => ns.ps(server).forEach(script => script.filename === fileName && ns.kill(script.pid));
-		for (let i = 0; i < ns.hacknet.numNodes(); i++) hns.push(`hacknet-node-${i}`); // hacknet servers are excluded
-		for (const server of getAllServers(ns)) {
-			const failChecks = server === "home" || hns.includes(server) || !ns.hasRootAccess(server) || ns.getServerMaxRam(server) < 2;
-			if (failChecks) continue;
-			if (ns.isRunning(fileName, server)) killIt(server, fileName);
-			const availableRam = ns.getServerMaxRam(server) - ns.getServerUsedRam(server), threads = Math.floor(availableRam / fileSize);
-			if (availableRam < 2) continue;
-			if (ns.scp(fileName, server) && ns.exec(fileName, server, threads)) ns.toast(`${fileName} started on ${server} with ${threads} threads.`);
+	ns.exec("superNuker.js", "home");
+	while (ns.isRunning("superNuker.js", ns.getHostname())) await ns.sleep(0);
+	ns.exec("marker.js", "home");
+	while (ns.isRunning("marker.js", ns.getHostname())) await ns.sleep(0);
+	if (ns.args.includes("tail")) ns.tail();
+
+	for (const server of getAllServers(ns)) {
+		for (const script of ns.ps(server)) {
+			if (script.pid === ns.pid) continue;
+			ns.kill(script.pid); // death to all the others.
 		}
-		killIt("home", fileName);
-		ns.spawn(fileName, Math.floor((ns.getServerMaxRam("home") + ns.getScriptRam(ns.getScriptName()) - ns.getServerUsedRam("home")) / fileSize));
+
+		if (!ns.hasRootAccess(server) || ns.getServerMaxRam(server) < 2) continue;
+		const availableRam = ns.getServerMaxRam(server) - ns.getServerUsedRam(server), threads = Math.floor(availableRam / 2);
+		if (availableRam < 2) continue;
+		if (ns.scp(fileName, server)) {
+			const p = ns.exec(fileName, server, threads);
+			if (p) {
+				ns.toast(`PID: (${p}) File: ${fileName} started on ${server} with ${threads} threads.`);
+				workers.push(p);
+			}
+		}
+	}
+
+	ns.atExit(() => {
+		workers.forEach(w => ns.kill(w));
+		ns.write("giftLayouts/chargeTimestamp.txt", performance.now(), "w");
+		if (ns.args.includes("ai")) ns.exec("ai.js", "home"); // script name can be whatever you want ai.js is my start script
+	});
+
+	while (1) {
+		ns.clearLog();
+		ns.print(`PIDS: ${workers.join(", ")}.`);
+		await ns.sleep(1000); // to keep running
+		if (Number.isInteger(ns.args[0]) && performance.now() > startTime + ns.args[0]) ns.kill(ns.pid);
 	}
 }
