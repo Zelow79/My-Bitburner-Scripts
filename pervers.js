417d0cfff458d52ab1ad6f46fd14196c9e1e79d7
Zelow79 <zelow79@gmail.com>
2024-04-29 15:50:17 -0400

    Update pervers.js
    changed tail logic to match other scripts
    removed large number with 2 ** 30
    changed === to >= just incase the limit is a little off will still kill if the ram is higher than the asked for limit instead of exactly

--- pervers.js
+++ pervers.js
@@ -2,7 +2,7 @@ import { numPad, formatGB, format } from "ze-lib.js";
 /** @param {NS} ns */
 export async function main(ns) {
 	ns.disableLog('sleep'); ns.clearLog();
-	if (ns.args[1] !== false) ns.tail(); await ns.sleep(50); ns.resizeTail(420, 550);
+	if (ns.args.includes("tail")) ns.tail(); await ns.sleep(50); ns.resizeTail(420, 550);
 	while (1) {
 		while (ns.getPurchasedServerLimit() > ns.getPurchasedServers().length && ns.getPlayer().money > ns.getPurchasedServerCost(2)) {
 			ns.purchaseServer(`perver-${numPad(ns.getPurchasedServers().length, 2)}`, 2);
@@ -19,9 +19,9 @@ export async function main(ns) {
 		ns.print(`Servers Owned:       ${ns.getPurchasedServers().length}`);
 		for (const perver of ns.getPurchasedServers()) {
 			const cost = () => ns.getPurchasedServerUpgradeCost(perver, ns.getServer(perver).maxRam * 2);
-			ns.print(`${perver} RAM: ${formatGB(ns.getServer(perver).maxRam * 1073741824)} Upgrade Cost: ${format(cost())}`);
+			ns.print(`${perver} RAM: ${formatGB(ns.getServer(perver).maxRam * (2 ** 30))} Upgrade Cost: ${format(cost())}`);
 		}
-		if (!isNaN(ns.args[0]) && ns.getPurchasedServers().every(s => ns.getServer(s).maxRam === ns.args[0])
+		if (!isNaN(ns.args[0]) && ns.getPurchasedServers().every(s => ns.getServer(s).maxRam >= ns.args[0])
 			|| ns.getPurchasedServers().every(s => ns.getServer(s).maxRam >= ns.getPurchasedServerMaxRam())) ns.exit();
 		await ns.sleep(10);
 	}
