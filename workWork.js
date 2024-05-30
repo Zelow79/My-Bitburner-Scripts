ba4c7e152433b9dbfef7b4ff4a29b1b80b0e49ac
Zelow79 <zelow79@gmail.com>
2024-05-29 22:30:33 -0400

    Create workWork.js
    simple script for work management using the Jobs.js classes. accepts tail or a company name in the args to specify a company to apply to otherwise applies to all companies. then promotes all jobs then quits all jobs we have a faction for.

---
+++ workWork.js
@@ -0,0 +1,19 @@
+import { MyEmployment } from "Jobs.js";
+/** @param {NS} ns */
+export async function main(ns) {
+	ns.disableLog("ALL"); ns.clearLog();
+	if (ns.args.includes("tail")) ns.tail();
+	for (const invite of ns.singularity.checkFactionInvitations()) {
+		if (ns.singularity.joinFaction(invite)) ns.tprintRaw(`Accepted invite from ${invite}`);
+	}
+	const emp = new MyEmployment(ns);
+	if (ns.args.filter(e => e != "tail").length > 0) {
+		for (const arg of ns.args.filter(e => e != "tail")) {
+			emp.applyCompany(arg);
+		}
+	} else {
+		emp.apply4Factions();
+	}
+	emp.promoteMyJobs();
+	emp.takeOutTrash();
+}
