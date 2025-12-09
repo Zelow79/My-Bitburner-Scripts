6a231e4d35f896c26261861158172c0f76191311
Zelow79 <zelow79@gmail.com>
2025-12-08 21:06:45 -0500

    Update ibb.js
    Adjusted height, removed assassination spree checker and updated namespace for ns.ui.resizeTail() to replace ns.resizeTail() and other new ui methods since it's now depreciated.

--- ibb.js
+++ ibb.js
@@ -1,10 +1,9 @@
 import { art, format, formatPercent, hms, cities, tem } from "ze-lib";
 /** @param {NS} ns */
 export async function main(ns) {
-	const [width, height] = [215, 710], b = ns.bladeburner;
-	ns.tail(); ns.disableLog("ALL"); ns.resizeTail(width, height); ns.clearLog();
-	ns.setTitle(tem("🔍BladeBurner:Info", { "font-family": 'Brush Script MT, cursive' }));
-	let lastKnownAssSpree;
+	const [width, height] = [215, 690], b = ns.bladeburner;
+	ns.ui.openTail(); ns.disableLog("ALL"); ns.ui.resizeTail(width, height); ns.clearLog();
+	ns.ui.setTailTitle(tem("🔍BB:Info", { "font-family": 'Brush Script MT, cursive' }));
 
 	while (1) {
 		await ns.sleep(500);
@@ -13,7 +12,7 @@ export async function main(ns) {
 	}
 
 	function printInfo() {
-		ns.resizeTail(width, height); ns.clearLog();
+		ns.ui.resizeTail(width, height); ns.clearLog();
 		const divider = "══════════════════════════════";
 		ns.print("BLADEBURNER INFORMATION");
 		const currentTask = b.getCurrentAction(),
@@ -25,7 +24,6 @@ export async function main(ns) {
 		const stamina = b.getStamina()[0] / b.getStamina()[1];
 		ns.print(` ${art("┗", { color: 255 })}Stamina:    ${art(formatPercent(stamina), { color: 255 })}`);
 		const assCount = b.getActionCountRemaining("Operations", "Assassination");
-		if (assCount == 0) lastKnownAssSpree = Date.now(); // timestamp spree start time
 		const assLevel = b.getActionCurrentLevel("Operations", "Assassination"),
 			maxAssLevel = b.getActionMaxLevel("Operations", "Assassination");
 		ns.print(divider);
@@ -38,8 +36,7 @@ export async function main(ns) {
 		const successesToLevel = Math.floor(0.5 * (maxAssLevel) * (2 * 2.5 + (maxAssLevel - 1))) - successes, // number of additional successes needed for next level up
 			successesLeftToGet = successesToLevel - assCount; // factor successes needed after subtracting current assassination count
 		ns.print(` ${art("┗", { color: 255 })}Success to lv: ${art(format(successesToLevel, 2), { color: 255 })}${successesLeftToGet > 0 ? art("(" + format(successesLeftToGet, 2) + ")", { color: 10 }) : ""}`);
-		const assMs = (lastKnownAssSpree) ? `*${art(hms((Math.floor((Date.now() - lastKnownAssSpree))), '00:00:00'), { color: 255 })} since last spree` : "";
-		ns.print(assMs); ns.print(divider);
+		ns.print(divider);
 		const unspentPoints = b.getSkillPoints(),
 			rank = b.getRank(),
 			skillLevel = x => b.getSkillLevel(x);
